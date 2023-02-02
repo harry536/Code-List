e9f9b273cff2025949c1968c6bf4811356587fda
Harendra singh <72804705+harry536@users.noreply.github.com>
2023-02-02 10:53:19 +0530

    Time: 7 ms (59.43%), Space: 9.6 MB (49.27%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     bool isAlienSorted(vector<string>& words, string order) {
-        unordered_map<char,int> mp;
+        unordered_map<int,int> mp;
         for(int i=1;i<=26;i++){
-            mp[order[i-1]] = i;
+            mp[order[i-1]-'a'] = i;
+        }
+        for(auto i : mp){
+            cout<<i.first<<" "<<i.second<<" ";
         }
         for(int i=0;i<words.size();i++){
             for(int j=0;j<words[i].size();j++){
-                words[i][j] = mp[words[i][j]];
+                words[i][j] = mp[words[i][j]-'a'];
+                
+                // cout<<words[i][j]<<" ";
             }
         }
         return is_sorted(words.begin(),words.end());
