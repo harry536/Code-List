29b32e9167d7f4a5826c181f8d055d40a7637631
Harendra singh <72804705+harry536@users.noreply.github.com>
2023-03-14 09:50:24 +0530

    Time: 3 ms (62.17%), Space: 9.3 MB (52.62%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.cpp
@@ -14,12 +14,12 @@ public:
     int sumNumbers(TreeNode* root) {
         return dfs(root, 0);
     }
-    // preorder dfs traversal
+    
     int dfs(TreeNode* root, int cur) {
         if(!root) return 0;
-        cur = cur * 10 + root -> val;                 // append current node's digit
-        if(!root -> left && !root -> right)           // leaf node - return current number to be added to total sum
+        cur = cur * 10 + root -> val;                 
+        if(!root -> left && !root -> right)           
             return cur;
-        return dfs(root -> left, cur) + dfs(root -> right, cur);   // recurse for child if current node is not leaf
-    }  
+        return dfs(root -> left, cur) + dfs(root -> right, cur); 
+    }
 };
